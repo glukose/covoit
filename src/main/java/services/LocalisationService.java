518ced64f131ebf9a37501e3f31f335d16ce22f6
Xarcan <charlycarrere@gmail.com>
2013-03-29 12:14:30 +0100

    modif de la classe LoacalisationService

--- src/main/java/services/LocalisationService.java
+++ src/main/java/services/LocalisationService.java
@@ -1,6 +1,11 @@
 package services;
 
+import java.util.Vector;
+
+import domain.Prof;
+
 public interface LocalisationService {
 
+	public Vector<Prof> localiserVoisin(Double latitude, Double longitude, Integer perimetre);
 
 }
